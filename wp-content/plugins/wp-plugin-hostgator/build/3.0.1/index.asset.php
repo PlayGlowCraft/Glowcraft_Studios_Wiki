3ef165b32445fda146997935b3f5d7a43038e62c
GlowCraft <123037413+PlayGlowCraft@users.noreply.github.com>
2026-04-12 02:10:29 -0500

    wiki move

--- wp-content/plugins/wp-plugin-hostgator/build/3.0.1/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-url'), 'version' => 'f38f1185d85d11842c91');
--- wp-content/plugins/wp-plugin-hostgator/build/3.0.1/portal-registry.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '9bd61e6ac4181630c28c');
